c772597cb408f161bfabcd08a9daa4344477a9f3
Taron Suvaryan <tsuvyerevan@gmail.com>
2024-03-05 03:43:57 -0800

    FlatList and SectionList

--- App.tsx
+++ App.tsx
@@ -2,75 +2,125 @@ import React, {useState} from 'react';
 import {
   RefreshControl,
   ScrollView,
-  StyleSheet,
+  View,
   Text,
-  View
-} from "react-native";
+  StyleSheet,
+  FlatList,
+  SectionList,
+} from 'react-native';
 
 const App = () => {
   const [items, setItems] = useState([
-    {key: 1, item: 'Item 1'},
-    {key: 2, item: 'Item 2'},
-    {key: 3, item: 'Item 3'},
-    {key: 4, item: 'Item 4'},
-    {key: 5, item: 'Item 5'},
-    {key: 6, item: 'Item 6'},
-    {key: 7, item: 'Item 7'},
-    {key: 8, item: 'Item 8'}
+    {name: 'Item 1'},
+    {name: 'Item 2'},
+    {name: 'Item 3'},
+    {name: 'Item 4'},
+    {name: 'Item 5'},
+    {name: 'Item 6'},
+    {name: 'Item 7'},
+    {name: 'Item 8'},
+    {name: 'Item 9'},
+    {name: 'Item 10'},
   ]);
-  const [refreshing, setRefreshing] = useState(false)
 
-  const onRefresh = ()=> {
+  const DATA = [
+    {
+      title: 'Title 1',
+      data: ['Item 1-1', 'Item 1-2', 'Item 1-3'],
+    },
+    {
+      title: 'Title 2',
+      data: ['Item 2-1', 'Item 2-2', 'Item 2-3'],
+    },
+    {
+      title: 'Title 3',
+      data: ['Item 3-1'],
+    },
+    {
+      title: 'Title 4',
+      data: ['Item 4-1', 'Item 4-2'],
+    },
+  ];
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
     setRefreshing(true);
-    setItems([...items, {key:9, item: 'Item 9'}]);
+    setItems([...items, {name: 'Item 9'}]);
     setRefreshing(false);
-  }
+  };
 
   return (
-    <View style={styles.body}>
-        <ScrollView
-          refreshControl={
-           <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-            colors={['#4ae1fa']}
-           />
-          }
-        >
-          <View>
-            {
-              items.map((object) => {
-                return(
-                  <View style={styles.item} key={object.key}>
-                    <Text style={styles.text}>{object.item}</Text>
-                  </View>
-                )
-              })
-            }
-          </View>
-        </ScrollView>
-    </View>
+    <SectionList
+      keyExtractor={(item, index) => index.toString()}
+      sections={DATA}
+      renderItem={({ item }) => (
+        <Text style={styles.text}>{item}</Text>
+      )}
+      renderSectionHeader={({section})=>(
+        <View style={styles.item}>
+          <Text style={styles.text}>{section.title}</Text>
+        </View>
+      )}
+    />
+
+    // <FlatList
+    //   keyExtractor={(item, index) => index.toString()}
+    //   data={items}
+    //   renderItem={({ item }) => (
+    //     <View style={styles.item}>
+    //       <Text style={styles.text}>{item.name}</Text>
+    //     </View>
+    //   )}
+    //   refreshControl={
+    //     <RefreshControl
+    //       refreshing={refreshing}
+    //       onRefresh={onRefresh}
+    //       colors={['#ff00ff']}
+    //     />
+    //   }
+    // />
+    // <View style={styles.body}>
+    //   <ScrollView
+    //     refreshControl={
+    //       <RefreshControl
+    //         refreshing={refreshing}
+    //         onRefresh={onRefresh}
+    //         colors={['#4ae1fa']}
+    //       />
+    //     }>
+    //     <View>
+    //       {items.map(object => {
+    //         return (
+    //           <View style={styles.item} key={object.key}>
+    //             <Text style={styles.text}>{object.item}</Text>
+    //           </View>
+    //         );
+    //       })}
+    //     </View>
+    //   </ScrollView>
+    // </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
   },
   item: {
     margin: 10,
     backgroundColor: '#4ae1fa',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   text: {
     color: '#000000',
     fontSize: 45,
     fontStyle: 'italic',
-    margin:10
-  }
+    margin: 10,
+  },
 });
 
 export default App;
