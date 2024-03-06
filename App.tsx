0d35dc2cad61f5e9a5b90c01eb00ceb1e366e633
Taron Suvaryan <tsuvyerevan@gmail.com>
2024-03-06 02:08:14 -0800

    Modal

--- App.tsx
+++ App.tsx
@@ -9,26 +9,57 @@ import {
   TouchableHighlight,
   TouchableWithoutFeedback,
   Pressable,
-  Alert
+  Alert,
+  Modal,
 } from 'react-native';
 
 const App = () => {
   const [name, setName] = useState('');
   const [submitted, setSubmitted] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const onPressHandler = () => {
-    if(name.length > 3) {
+    if (name.length > 3) {
       setSubmitted(!submitted);
-    }else {
-      Alert.alert('Warning', 'The name must be longer than 3 characters', [
-        {text: 'Do not show again', onPress:() => console.warn('Do not show again'), style: 'destructive'},
-        {text: 'Cancel', onPress:() => console.warn('Cancel Pressed!'), style: 'destructive'},
-        {text: 'OK', onPress:() => console.warn('OK Pressed'), style: 'destructive'},
-      ], {cancelable: true, onDismiss:() => console.warn('Alert dismissed!')});
+    } else {
+      // Alert.alert('Warning', 'The name must be longer than 3 characters', [
+      //   {text: 'Do not show again', onPress:() => console.warn('Do not show again'), style: 'destructive'},
+      //   {text: 'Cancel', onPress:() => console.warn('Cancel Pressed!'), style: 'destructive'},
+      //   {text: 'OK', onPress:() => console.warn('OK Pressed'), style: 'destructive'},
+      // ], {cancelable: true, onDismiss:() => console.warn('Alert dismissed!')});
+      setShowModal(true);
     }
   };
 
   return (
     <View style={styles.body}>
+      <Modal
+        visible={showModal}
+        transparent
+        onRequestClose={() => setShowModal(false)}
+        animationType='fade'
+      >
+        <View style={styles.centered_view}>
+          <View style={styles.warning_modal}>
+            <View style={styles.warning_title}>
+              <Text style={styles.text}>Warning</Text>
+            </View>
+            <View style={styles.warning_body}>
+              <Text style={styles.text}>
+                The name must be longer that 3 characters
+              </Text>
+            </View>
+            <Pressable
+              onPress={() => setShowModal(false)}
+              style={styles.warning_butoom}
+              android_ripple={{color: '#fff'}}
+            >
+              <Text style={styles.text}>
+                Ok
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <Text style={styles.text}>Please write</Text>
       <TextInput
         multiline
@@ -102,6 +133,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontStyle: 'italic',
     margin: 10,
+    textAlign: 'center',
   },
   input: {
     width: 200,
@@ -117,6 +149,38 @@ const styles = StyleSheet.create({
     height: 50,
     alignItems: 'center',
   },
+  centered_view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: '#000000'
+  },
+  warning_modal: {
+    width: 300,
+    height: 300,
+    // backgroundColor: '#999',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 20,
+  },
+  warning_title: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ff0',
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  warning_body: {
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  warning_butoom: {
+    backgroundColor: '#00ffff',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20
+  }
 });
 
 export default App;
